31c242392c34b509e346b8795cbb762e5cf7dcfd
ryandgs <ryandouglasdasilva@outlook.com>
2025-01-25 00:27:20 -0300

    feat(landing) - add info sections

---
+++ resources/views/components/landing/info-section-l.blade.php
@@ -0,0 +1,13 @@
+<div class="h-[600px] w-full flex justify-center items-center flex-col relative bg-[url('{{ asset('/assets/background-hero.webp') }}')] bg-no-repeat bg-[center_right_70vw] bg-[length:auto_70%]">
+    <div class="absolute inset-0 bg-gradient-to-b from-black/80 via-black/80 to-black to-90%"></div>
+    <div class="h-full text-left container mx-auto relative z-10 flex flex-col justify-center gap-y-4 py-16">
+        <div class="md:grid grid-cols-4">
+            <div class="col-span-1"></div>
+            <div class="col-span-3">
+                <span class="text-[#7B5AFF] text-3xl font-bold font-secondary">Titulo foda</span>
+                <h1 class="text-text-high text-sm font-bold font-secondary first-letter:uppercase">Lorem ipsum</h1>
+                <p class="text-text-medium font-primary font-semibold leading-5">Ut harum quia doloremque ex in earum. Ad et voluptatibus consequatur. Sequi non omnis nulla numquam qui. Sapiente id quae qui sit voluptatem quis reprehenderit. Nihil aut voluptates nesciunt beatae. Ut harum quia doloremque ex in earum. Ad et voluptatibus consequatur. Sequi non omnis nulla numquam qui. Sapiente id quae qui sit voluptatem quis reprehenderit. Nihil aut voluptates nesciunt beatae. Ut harum quia doloremque ex in earum. Ad et voluptatibus consequatur. Sequi non omnis nulla numquam qui.</p>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
     <x-landing.speakers />
     <x-landing.highlights />
     <x-landing.cta-invite />
+    <x-landing.info-section-r />
+    <x-landing.info-section-l />
     <x-landing.discover-city />
     <x-landing.footer />
 </x-app-layout>
